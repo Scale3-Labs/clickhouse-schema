646f36c37fdb3f9fd231b9ff8bcc9a33c7946140
Rohit Kadhe <rohit@scale3labs.com>
2024-03-25 09:01:37 -0600

    update readme and export in index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ export { ClickhouseSchema } from '@clickhouse-schema-core/clickhouse_schema'
 export { ClickhouseTypes } from '@clickhouse-schema-data-types/index'
 export type { InferClickhouseSchemaType } from '@clickhouse-schema-core/infer_schema_type'
 export {
-  CHJSON, CHFloat32, CHFloat64,
+  CHJSON, CHFloat32, CHFloat64, CHDecimal,
   CHBoolean, CHUUID, CHArray, CHDate,
   CHDate32, CHDateTime, CHDateTime64,
   CHEnum, CHFixedString, CHInt16, CHInt8,
